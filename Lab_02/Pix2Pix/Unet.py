096ff97725f3f9a765a3ff887355df75f9dfdccc
ParanoidAndroid-678 <110105571+ParanoidAndroid-678@users.noreply.github.com>
2024-10-30 16:53:50 +0800

    Create Unet.py

---
+++ Lab_02/Pix2Pix/Unet.py
@@ -0,0 +1,66 @@
+import torch.nn as nn
+
+class FullyConvNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Encoder (Convolutional Layers)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        # Decoder (Deconvolutional Layers)
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True) 
+        )
+
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(inplace=True)
+        )
+
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh() # Use Tanh activation function to ensure output values are between -1 and 1
+        )
+
+    def forward(self, x):
+        # Encoder forward pass
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+
+        # Decoder forward pass
+        x5 = self.deconv1(x4)
+        x6 = self.deconv2(x5 + x3)
+        x7 = self.deconv3(x6 + x2)
+        x8 = self.deconv4(x7 + x1)
+
+        output = x8
+        return output
